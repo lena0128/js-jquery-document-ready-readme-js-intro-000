a01ef7636c94c591a0bc3d892990a367873c7322
Lena Hou <lenahou2013@gmail.com>
2020-12-22 21:31:28 +0000

    Automatically backed up by Learn

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-$(document).ready(function()) {
-
-  $("#text").append("this is so freaking cool.");
-
+$(document).ready(function() {
+    // code to be executed goes here
+    $("#text").append("this is so freaking cool.");
 });
